93a622fc9ed1dbbd0d537ead919ee5249cfa90dd
Leon Winde <leon.winde@dci-student.org>
2025-04-30 20:45:18 +0200

    fixed a ts error i ignored

--- lib/send-mail.ts
+++ lib/send-mail.ts
@@ -1,17 +1,17 @@
-'use server';
-import nodemailer from 'nodemailer';
+"use server";
+import nodemailer from "nodemailer";
 const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
 const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
 // const SITE_MAIL_RECIEVER = process.env.SITE_MAIL_RECIEVER;
 const transporter = nodemailer.createTransport({
-    host: SMTP_SERVER_HOST || 'smtp.gmail.com',
-    port: 587,
-    secure: false, // In Germany, port 587 with secure set to false is commonly used for STARTTLS
-    auth: {
-        user: SMTP_SERVER_USERNAME,
-        pass: SMTP_SERVER_PASSWORD,
-    },
+  host: SMTP_SERVER_HOST || "smtp.gmail.com",
+  port: 587,
+  secure: false, // In Germany, port 587 with secure set to false is commonly used for STARTTLS
+  auth: {
+    user: SMTP_SERVER_USERNAME,
+    pass: SMTP_SERVER_PASSWORD,
+  },
 });
 
 export async function sendMail({
@@ -28,19 +28,25 @@ export async function sendMail({
   html?: string;
 }) {
   try {
-    const isVerified = await transporter.verify();
+    // const isVerified =
+    await transporter.verify();
   } catch (error) {
-    console.error('Something Went Wrong', SMTP_SERVER_USERNAME, SMTP_SERVER_PASSWORD, error);
+    console.error(
+      "Something Went Wrong",
+      SMTP_SERVER_USERNAME,
+      SMTP_SERVER_PASSWORD,
+      error
+    );
     return;
   }
   const info = await transporter.sendMail({
     from: email,
-    to: sendTo , //|| SITE_MAIL_RECIEVER
+    to: sendTo, //|| SITE_MAIL_RECIEVER
     subject: subject,
     text: text,
-    html: html ? html : '',
+    html: html ? html : "",
   });
-  console.log('Message Sent', info.messageId);
-  console.log('Mail sent to', sendTo);
+  console.log("Message Sent", info.messageId);
+  console.log("Mail sent to", sendTo);
   return info;
 }
