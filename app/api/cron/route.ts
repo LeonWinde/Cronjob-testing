cf7667228ded6f1f133c449cb89ba4ae128457bd
Leon Winde <leon.winde@dci-student.org>
2025-05-07 00:42:55 +0200

    added the email back

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,12 +1,12 @@
-// import { sendMail } from "@/lib/send-mail";
+import { sendMail } from "@/lib/send-mail";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // sendMail({
-  //   email: "leon.winde@gmail.com",
-  //   subject: "testing",
-  //   text: "testing",
-  // });
+  sendMail({
+    email: "leon.winde@gmail.com",
+    subject: "testing",
+    text: "testing",
+  });
   console.log("tesint");
   return NextResponse.json({ ok: true });
 }
