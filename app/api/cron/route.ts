62dc197a1f84e6ead87a0a1fd9a2ff25991e8c8c
Leon Winde <leon.winde@dci-student.org>
2025-05-07 01:30:07 +0200

    added more testing

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,11 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   console.log("Sending mail...")
-  sendMail({
-    email: "leon.winde@gmail.com",
-    subject: "testing",
-    text: "testing",
-  });
+  try {
+    await sendMail({
+      email: "leon.winde@gmail.com",
+      subject: "testing",
+      text: "testing",
+    });
+  } catch (error) {
+    console.log(error);
+  }
   return NextResponse.json({ ok: true });
 }
 
