7840e93d409d080bf85afc9060a7454fce8475d5
Leon Winde <leon.winde@dci-student.org>
2025-05-07 01:11:09 +0200

    still just testing

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,14 +2,11 @@ import { sendMail } from "@/lib/send-mail";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  
-
   sendMail({
     email: "leon.winde@gmail.com",
     subject: "testing",
     text: "testing",
   });
-  console.log("tesint");
   return NextResponse.json({ ok: true });
 }
 
